0b1e6b650b1ca9e6b7367d7f0de085a6d0068cba
Beni Ben Zikry <bbenzikry@gmail.com>
2021-04-05 01:13:53 +0300

    lua update

--- Formula/lua-language-server.rb
+++ Formula/lua-language-server.rb
@@ -1,6 +1,6 @@
 class LuaLanguageServer < Formula
-  url "https://github.com/sumneko/vscode-lua/archive/refs/tags/v1.20.1.tar.gz"
-  sha256 "3cc8fcdb99c425c86d0e47c8924cce318cb7ebf4a951d3da2fed139dbf97311d"
+  url "https://github.com/sumneko/vscode-lua/archive/refs/tags/v1.20.2.tar.gz"
+  #  sha256 "3cc8fcdb99c425c86d0e47c8924cce318cb7ebf4a951d3da2fed139dbf97311d"
   def install
     opoo "This is a private tap, use at your own peril"
     bin.install "./bin/macOS/lua-language-server"
